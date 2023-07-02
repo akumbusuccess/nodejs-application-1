367e37a5ad8562532c79b5c52d0355f0d517a54b
Olorunfemi Kawonise <femi@MacBook-Pro.local>
2023-07-02 15:44:47 -0400

    npmrc file changre

--- nexus.sh
+++ nexus.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/expect -f
 
 # Set the credentials
-sh set username "admin"
-sh set password "admin123"
+set username "admin"
+set password "admin123"
 
 # Run the npm login command with expect
-sh spawn npm login --registry=http://52.207.254.164:8081/repository/fintech-app-release/
+spawn npm login --registry=http://52.207.254.164:8081/repository/fintech-app-release/
 
 # Expect the username prompt and provide the username
-sh expect "Username:"
-sh send "$username\r"
+expect "Username:"
+send "$username\r"
 
 # Expect the password prompt and provide the password
-sh expect "Password:"
-sh send "$password\r"
+expect "Password:"
+send "$password\r"
 
 # Expect the email prompt and provide the email (if required)
-sh expect "Email: (this IS public)"
-sh send "\r"
+expect "Email: (this IS public)"
+send "\r"
 
 # Wait for the login process to complete
-sh expect eof
+expect eof
