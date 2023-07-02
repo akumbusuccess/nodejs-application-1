23e8b83356c492380a3fa682aad37eb5c9915a26
Olorunfemi Kawonise <femi@MacBook-Pro.local>
2023-07-02 15:49:28 -0400

    npmrc file changre

--- scripts/runTests.sh
+++ scripts/runTests.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env sh
 
-echo 'The following "npm" command will run the test cases.'
+# Set the credentials
+set username "admin"
+set password "admin123"
 
-npm test
+# Run the npm login command with expect
+spawn npm login --registry=http://52.207.254.164:8081/repository/fintech-app-release/
+
+# Expect the username prompt and provide the username
+expect "Username:"
+send "$username\r"
+
+# Expect the password prompt and provide the password
+expect "Password:"
+send "$password\r"
+
+# Expect the email prompt and provide the email (if required)
+expect "Email: (this IS public)"
+send "\r"
+
+# Wait for the login process to complete
+expect eof
