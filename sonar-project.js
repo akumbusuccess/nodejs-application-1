c8a34ee6e0ce080ffba1d35d171bbc5b7efee202
Olorunfemi Kawonise <femi@MacBook-Pro.local>
2023-07-04 11:06:51 -0400

    sonarcloud integration

--- sonar-project.js
+++ sonar-project.js
@@ -2,11 +2,12 @@ const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
   serverUrl: 'https://sonarcloud.io/',
+  token: 'ddb7e6a8da056e85ade9d762fbf1ca6f4a464409',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': 'ddb7e6a8da056e85ade9d762fbf1ca6f4a464409',
+	    // 'sonar.login': 'ddb7e6a8da056e85ade9d762fbf1ca6f4a464409',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
