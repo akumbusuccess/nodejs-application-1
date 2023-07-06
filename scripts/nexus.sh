9e23b4707ce55f3f9b030a562bfc4a5c13a5a297
Olorunfemi Kawonise <femi@MacBook-Pro.local>
2023-07-06 18:31:50 -0400

    upload to nexus

--- scripts/nexus.sh
+++ scripts/nexus.sh
@@ -1,5 +1,5 @@
 npm config list
-npm config set registry http://44.212.24.102:8081/repository/fintech-app-release/
+npm config set registry http://44.212.72.121:8081/repository/fintech-app-release/
 
 # Provide your Nexus registry username and password
 username=admin
